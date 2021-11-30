d9b2a009d490a86d05a6faefa78c9fe153441e16
ElisaOppe <91607662+ElisaOppe@users.noreply.github.com>
2021-11-30 11:53:03 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+//
+// Created by elisa on 30/11/2021.
+//
 
+#ifndef ESERCITAZIONE6DEL02_12_2021_LIB_H
+#define ESERCITAZIONE6DEL02_12_2021_LIB_H
+
+#endif //ESERCITAZIONE6DEL02_12_2021_LIB_H
+
+bool conversione (char&);
